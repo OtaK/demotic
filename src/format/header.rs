35788450d9a9b96a053d70368f56e074060b62a6
Mathieu Amiot <m.amiot@otak-arts.com>
2020-04-02 02:37:33 +0200

    WIP, first working thing

--- src/format/header.rs
+++ src/format/header.rs
@@ -1,10 +1,7 @@
+use crate::format::take_source_str;
 use nom::{
     do_parse,
     tag,
-    named,
-    map,
-    map_res,
-    take_till,
     number::streaming::{le_u32, le_f32}
 };
 
@@ -17,12 +14,11 @@ pub struct DemoHeader {
     map_name: String,
     directory: String,
     playback_time: f32,
+    tickrate: u32,
     ticks: u32,
     frames: u32,
     signon_len: u32,
 }
-
-named!(till_eol, take_till!(|c| c == b'\n'));
 
 impl crate::Parsable for DemoHeader {
     fn parse(i: &[u8]) -> nom::IResult<&[u8], Self> {
@@ -30,10 +26,10 @@ impl crate::Parsable for DemoHeader {
             tag!(b"HL2DEMO\0")              >>
             demo_protocol: le_u32           >>
             network_protocol: le_u32        >>
-            server_name: map!(map_res!(till_eol, std::str::from_utf8), String::from)    >>
-            client_name: map!(map_res!(till_eol, std::str::from_utf8), String::from)    >>
-            map_name: map!(map_res!(till_eol, std::str::from_utf8), String::from)       >>
-            directory: map!(map_res!(till_eol, std::str::from_utf8), String::from)      >>
+            server_name: take_source_str    >>
+            client_name: take_source_str    >>
+            map_name: take_source_str       >>
+            directory: take_source_str      >>
             playback_time: le_f32           >>
             ticks: le_u32                   >>
             frames: le_u32                  >>
@@ -42,10 +38,30 @@ impl crate::Parsable for DemoHeader {
                 demo_protocol, network_protocol,
                 server_name, client_name,
                 map_name, directory,
+                tickrate: (ticks as f32 / playback_time) as u32,
                 playback_time,
                 ticks, frames,
                 signon_len
             })
         )
+    }
+}
+
+mod tests {
+    #[test]
+    fn can_parse_header() {
+        use std::io::BufRead;
+        use super::DemoHeader;
+        use crate::Parsable as _;
+
+        println!("Parsing: natus-vincere-vs-vitality-m2-dust2.dem");
+        let demo_file = std::fs::File::open("assets/natus-vincere-vs-vitality-m2-dust2.dem").unwrap();
+        let mut reader = std::io::BufReader::new(demo_file);
+        let mut buf = Vec::with_capacity(1024);
+        reader.read_until(b'\n', &mut buf).unwrap();
+
+        let (_, header) = DemoHeader::parse(&buf).unwrap();
+
+        println!("Header {:#?}", header);
     }
 }
--- src/format/mod.rs
+++ src/format/mod.rs
@@ -1,6 +1,14 @@
+
+
 mod header;
 mod packet;
 mod frame;
+
+pub(crate) fn take_source_str(i: &[u8]) -> nom::IResult<&[u8], String> {
+    let (i, sstr) = nom::combinator::map_res(nom::bytes::streaming::take(260usize), std::str::from_utf8)(i)?;
+    let ret = String::from(sstr.trim_end().trim_end_matches('\0'));
+    Ok((i, ret))
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Demo {
